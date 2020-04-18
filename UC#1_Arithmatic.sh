524cb6fc5d3f767811077c1ea426d5b5c102bb63
DattaDhebe <dattadhebe75@gmail.com>
2020-04-18 13:12:17 +0530

    [UseCase6] All the use case result Dictionary

--- UC#1_Arithmatic.sh
+++ UC#1_Arithmatic.sh
@@ -4,4 +4,17 @@ read -p "Enter first number : " a
 read -p "Enter second number : " b
 read -p "Enter Third Number : " c
 
-echo $(($a % $b + $c))
+UseCase2=$(($a + $b * $c))
+
+UseCase3=$(($a * $b + $c))
+
+UseCase4=$(($c * $a / $b))
+
+UseCase5=$(($a % $b + $c))
+
+compute[uc1]=$UseCase2
+compute[uc2]=$UseCase3
+compute[uc3]=$UseCase4
+compute[uc4]=$UseCase5
+
+echo "${compute[@]}"
