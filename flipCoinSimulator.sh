6e86c59e775eacd5ee45a6690f00d0ba12dcc0d2
DattaDhebe <dattadhebe75@gmail.com>
2020-04-19 17:08:56 +0530

    [useCase2] Add data in dictionary and find the percentage of head and tail

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,10 +1,28 @@
 #!/bin/bash -x
 
-flip=$(( RANDOM % 2 ))
+read -p "How many Time you Want to flip coin :"  count
 
-if [[ $flip = 1 ]]
-then
-	echo head
-else
-	echo tail
-fi
+declare -A dictionary
+
+#declaration of head and tail
+head=0;
+tail=0;
+
+for ((i=1; i<=count; i++))
+do
+	flip=$(( RANDOM % 2 ))
+	if [ $flip -eq 1 ]
+	then
+		head=$(($head + 1));
+	else
+		tail=$(($tail + 1));
+	fi
+	
+	dictionary[$i]=$flip;
+
+done
+
+head_percentage=$(( $head * 100 / $count ))
+tail_percentage=$(( $tail * 100 / $count ))
+
+
