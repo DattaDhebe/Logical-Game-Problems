2911c60956572c4af07435574ba9945fbae410ec
DattaDhebe <dattadhebe75@gmail.com>
2020-04-19 20:43:19 +0530

    [useCase4] Added condition of 20 days of playing and total amount

--- gamblingSimulation.sh
+++ gamblingSimulation.sh
@@ -1,21 +1,41 @@
 #!/bin/bash -x
 
 stake=100;
+days=0;
 
-while [ true ]
-do
-	gamble=$(( RANDOM % 2 ))
-	if [[ $stake > 50 || $stake < 150 ]]
-	then	
-		if [ $gamble = 1 ]
-		then
-			stake=$(($stake + 1));
+function gamblingSimulation() {
+   while [ true ]
+   do
+      gamble=$(( RANDOM % 2 ))
+      if [[ $stake > 50 || $stake < 150 ]]
+      then  
+        	if [ $gamble = 1 ]
+        	then
+           	stake=$(($stake + 1));
+        	else
+           	stake=$(($stake - 1));
+        	fi 
 		else
-			stake=$(($stake - 1));
+			break;
 		fi
-	else
-		break;
-	fi
-done
+	done
+	echo "$stake"
+}
 
-echo $stake
+function useCase4() {
+	while [ true ]
+	do
+		((days++))
+		if [ $days -lt 20 ]
+		then	
+			perDayStake="$( gamblingSimulation $stake )"
+			total_amount=$(($total_amount + $perDayStake))
+		else
+			break;
+		fi
+	done
+}
+
+useCase4
+echo $total_amount
+
