b6a60f16fbec612ee2235fcfe73c1cc76fdf549a
DattaDhebe <dattadhebe75@gmail.com>
2020-04-19 19:11:09 +0530

    [useCase2] Added number of days to play as well as change in stake

--- gamblingSimulation.sh
+++ gamblingSimulation.sh
@@ -1,11 +1,18 @@
 #!/bin/bash -x
 
-stake=100;
-gamble=$(( RANDOM % 2 ))
+read -p "Enter number of Day to Play :" count
 
-if [ $gamble = 1 ]
-then
-	echo win
-else
-	echo loss
-fi
+stake=100;
+
+for ((i=1; i<=count; i++))
+do
+	gamble=$(( RANDOM % 2 ))
+	if [ $gamble = 1 ]
+	then
+		echo win
+		stake=$(($stake + 1))
+	else
+		echo loss
+		stake=$(($stake - 1))
+	fi
+done
