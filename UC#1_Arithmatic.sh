c31fbe7ab8e10397fbf1e9b6c57f8a69af069613
DattaDhebe <dattadhebe75@gmail.com>
2020-04-19 16:20:12 +0530

    [useCase8] sort result and show in descending order

--- UC#1_Arithmatic.sh
+++ UC#1_Arithmatic.sh
@@ -4,27 +4,45 @@ read -p "Enter first number : " a
 read -p "Enter second number : " b
 read -p "Enter Third Number : " c
 
-UseCase2=$(($a + $b * $c))
+UseCase2=$(($a + $b * $c));
+UseCase3=$(($a * $b + $c));
+UseCase4=$(($c * $a / $b));
+UseCase5=$(($a % $b + $c));
 
-UseCase3=$(($a * $b + $c))
+declare -A compute
 
-UseCase4=$(($c * $a / $b))
+compute[1]=$UseCase2;
+compute[2]=$UseCase3;
+compute[3]=$UseCase4;
+compute[4]=$UseCase5;
 
-UseCase5=$(($a % $b + $c))
 
-declare -A Compute
+dictionary=${compute[@]};
+echo $dictionary
 
-compute[computation1]=$UseCase2
-compute[computation2]=$UseCase3
-compute[computation3]=$UseCase4
-compute[computation4]=$UseCase5
-echo ${compute[@]}
+for ((count=1; count<=4; count++))
+do
+	arr[$count]=${compute[$count]};
+	echo ${arr[$count]};
+done
 
-UseCase[((counter++))]=$UseCase2
-UseCase[((counter++))]=$UseCase3
-UseCase[((counter++))]=$UseCase4
-UseCase[((counter++))]=$UseCase5
+for ((count=1; count<=4; count++))
+do
+	for ((inner_count=$count; inner_count<=4; inner_count++))
+	do
+		if [ ${arr[count]} -lt ${arr[inner_count]} ]
+		then
+			temp=${arr[count]};
+			arr[$count]=${arr[$inner_count]};
+			arr[$inner_count]=$temp;
+		fi
+	done
+done
 
-echo ${UseCase[@]}
+echo "Decsending order Result : "
+for((count=1; count<=4; count++))
+do
+	echo ${arr[$count]}
+done
 
 
