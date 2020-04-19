49285e5356eed7ea6adbc6ca30da5a7b2f41f3f6
DattaDhebe <dattadhebe75@gmail.com>
2020-04-19 18:36:45 +0530

    [useCase4] Store triplet combination as well as Percentage

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -62,3 +62,56 @@ function doubletCombination() {
 }
 
 doubletCombination $count
+
+#declare varible for triplet combination
+HHH=0;
+HHT=0;
+HTT=0;
+TTT=0;
+TTH=0;
+THH=0;
+HTH=0;
+THT=0;
+
+function tripleCombination() {
+	for ((i=1; i<=count; i++))
+	do
+		flip=$(( RANDOM % 8 ))
+		if [ $flip = 0 ]
+		then
+			HHH=$(($HHH + 1))
+		elif [ $flip = 1 ]
+		then
+			HHT=$(($HHT + 1))
+		elif [ $flip = 2 ]
+      then
+         HTT=$(($HTT + 1))
+		elif [ $flip = 3 ]
+      then
+         TTT=$(($TTT + 1))
+		elif [ $flip = 4 ]
+      then
+         TTH=$(($TTH + 1))
+		elif [ $flip = 5 ]
+      then
+         THH=$(($THH + 1))
+		elif [ $flip = 6 ]
+      then
+         HTH=$(($HTH + 1))
+		else
+			THT=$(($THT + 1))
+		fi
+	done
+	
+	HHH_percentage=$(($HHH * 100 / $count))
+	HHT_percentage=$(($HHT * 100 / $count))
+	HTT_percentage=$(($HTT * 100 / $count))
+	TTT_percentage=$(($TTT * 100 / $count))
+	TTH_percentage=$(($TTH * 100 / $count))
+	THH_percentage=$(($THH * 100 / $count))
+	HTH_percentage=$(($HTH * 100 / $count))
+	THT_percentage=$(($THT * 100 / $count))
+}
+
+tripleCombination $count
+
