44681dd3c463ed1cbc1c32ab0a26f28dfdee1e38
DattaDhebe <dattadhebe75@gmail.com>
2020-04-21 02:22:25 +0530

    Refactor. added array to store win or loss for a day

--- gamblingSimulation.sh
+++ gamblingSimulation.sh
@@ -1,23 +1,27 @@
 #!/bin/bash -x
 
 stake=100;
-
+count=1;
 function gamblingSimulation() {
    lowestLost=50;
 	highestWin=150;
+	
 	while [[ $stake -gt $lowestLost && $stake -lt $highestWin ]]
    do
+		((count++))
       gamble=$(( RANDOM % 2 ))
       if [ $gamble = 1 ]
       then
        	stake=$(($stake + 1));
+			winDay=$(($winDay + 1));
       else
         	stake=$(($stake - 1));
+			lossDay=$(($lossDay + 1))
       fi 
 		
 	done
-
-	echo $stake
+	winDayResult[$count]=$winDay;
+	lossDayResult[$count]=$lossDay;
 }
 
 #use case for for 20 days
@@ -28,18 +32,19 @@ numberOfMonth=12;
 function checkForDays() {
 	for ((month=1; month<=$numberOfMonth; month++))
 	do
-		for ((days=1; days<=$numbeOfDays; days++))
+		for ((days=1; days<=$numberOfDays; days++))
 		do
-			gamblingSimulation $stake
-			totalWinNLossPerDay=$(($winResult[$days] + $lossResult[$days]))
-			if [ $totalWinNLossPerDay -gt 100 ]
-			then
-				winDay=$(($totalWinNlossDay - 100))
-				echo $WinDay
-			else
-				lossDay=$((100 - $totalWinNlossDay ))
-				echo $lossDay
-			fi
+			amountPerDay=$(("gamblingSimulation $stake"))
+			echo ${winResult[$days]}
+			echo ${lossResult[$days]}
+		#	if [ $totalWinNLossPerDay -gt 100 ]
+		#	then
+		#		winDay=$(($totalWinNlossDay - 100))
+		#		echo $WinDay
+		#	else
+		#		lossDay=$((100 - $totalWinNlossDay ))
+		#		echo $lossDay
+		#	fi
 		done
 	done
 }
