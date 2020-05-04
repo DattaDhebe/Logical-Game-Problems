c9cf41eaddb519788adf08bf1a1d32fc0b2ea58f
DattaDhebe <dattadhebe75@gmail.com>
2020-05-04 13:54:55 +0000

    Refactor. added feature if percentage change then how to handle it

--- gamblingSimulation.sh
+++ gamblingSimulation.sh
@@ -3,20 +3,26 @@
 stake=100;
 winDay=0;
 lossDay=0;
+lowestLost=50;
+highestWin=150;
 
 declare -A perDayStakeAmount
+declare -A noOfWins
+declare -A noOfLoss
+
 for ((days=1; days<=20; days++))
 do
-   lowestLost=50;
-	highestWin=150;
+   
 	while [[ $stake -gt $lowestLost && $stake -lt $highestWin ]]
    do
 		gamble=$(( RANDOM % 2 ))
       if [ $gamble = 1 ]
       then
+       	noOfWins[$days]=$((${noOfWins[$days]}+1));
        	stake=$(($stake + 1));
 			winDay=$(($winDay + 1));
       else
+      	noOfLoss[$days]=$((${noOfLoss[$days]}+1));
         	stake=$(($stake - 1));
 			lossDay=$(($lossDay + 1))
       fi 
@@ -27,12 +33,16 @@ do
 	lossDayResult[$days]=$lossDay;
 	totalAmount=$(($totalAmount + $stake))
 	perDayStakeAmount["stake"]=$totalAmount
+	
+	lowestLost=$(($stake / 2));
+	highestWin=$(($stake + $lowestLost));
 done
+	
 
 echo "\nNumber of Days won : $winDay"
 echo "\nNumber of Days lost: $lossDay"
 
-if [ $winDayResult -gt $lossDayResult ] 
+if [ $ -gt $lossDayResult ] 
 then
 	echo "you can play next month\n"
 else
