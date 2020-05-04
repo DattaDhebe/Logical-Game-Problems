c48f9187793cfbe848ba6c2edffdbe5d9cc6c02a
DattaDhebe <dattadhebe75@gmail.com>
2020-05-04 14:08:42 +0000

    Refactor. Added code into function for reusability.

--- gamblingSimulation.sh
+++ gamblingSimulation.sh
@@ -3,48 +3,51 @@
 stake=100;
 winDay=0;
 lossDay=0;
-lowestLost=50;
-highestWin=150;
+
 
 declare -A perDayStakeAmount
 
+function gamblingSimulation() {
 
-for ((days=1; days<=20; days++))
-do
-   
-	while [[ $stake -gt $lowestLost && $stake -lt $highestWin ]]
-   do
-		gamble=$(( RANDOM % 2 ))
-      if [ $gamble = 1 ]
-      then
+	for ((days=1; days<=20; days++))
+	do
+   	lowestLost=$(($stake / 2));
+		highestWin=$(($stake + $lowestLost));
+		
+		while [[ $stake -gt $lowestLost && $stake -lt $highestWin ]]
+   	do
+			gamble=$(( RANDOM % 2 ))
+      	if [ $gamble = 1 ]
+      	then
        	
-       	stake=$(($stake + 1));
-			winDay=$(($winDay + 1));
-      else
+       		stake=$(($stake + 1));
+				winDay=$(($winDay + 1));
+      	else
       	
-        	stake=$(($stake - 1));
-			lossDay=$(($lossDay + 1))
-      fi 
+        		stake=$(($stake - 1));
+				lossDay=$(($lossDay + 1))
+      	fi 
+		
+		done
+	
+		winDayResult[$days]=$winDay;
+		lossDayResult[$days]=$lossDay;
+		totalAmount=$(($totalAmount + $stake))
+		perDayStakeAmount["stake"]=$totalAmount
+	
 		
 	done
 	
-	winDayResult[$days]=$winDay;
-	lossDayResult[$days]=$lossDay;
-	totalAmount=$(($totalAmount + $stake))
-	perDayStakeAmount["stake"]=$totalAmount
-	
-	lowestLost=$(($stake / 2));
-	highestWin=$(($stake + $lowestLost));
-done
-	
 
-echo "\nNumber of Days won : $winDay"
-echo "\nNumber of Days lost: $lossDay"
+	echo "\nNumber of Days won : $winDay"
+	echo "\nNumber of Days lost: $lossDay"
 
-if [ $winDayResult -gt $lossDayResult ] 
-then
-	echo "you can play next month\n"
-else
-	echo "you should not play next month\n"
-fi
+	if [ $winDayResult -gt $lossDayResult ] 
+	then
+		echo "you can play next month\n"
+	else
+		echo "you should not play next month\n"
+	fi
+}
 
+gamblingSimulation
